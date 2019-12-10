16fc108ae75cd221f27e89fad4774ada420cb4a9
Erik Kasperson <chefkasperson@gmail.com>
2019-12-10 06:12:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,12 @@
+# def square_array(array)
+#   squared = []
+#   array.each do |num|
+#     squared << num * num
+#   end
+# squared
+#   # your code here
+# end
+
 def square_array(array)
-  squared = []
-  array.each do |num|
-    squared << num * num
-  end
-squared
-  # your code here
+  array.collect {|n| n*n}
 end
